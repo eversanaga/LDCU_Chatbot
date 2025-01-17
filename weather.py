3e0cc895888b1d3c9e8ed3884220863983e43fec
69-ever-69 <esanaga15688@liceo.edu.ph>
2025-01-18 01:52:28 +0800

    LiceoChatV2 complete rework of LiceoChat

--- weather.py
+++ weather.py
@@ -1,10 +1,14 @@
 from requests import get
 
 def fetchWeather(APIKey) -> str:
-	output = get(f"http://api.openweathermap.org/data/2.5/weather?q=Cagayan%20De%20Oro&appid={APIKey}&units=metric")
-	if output.status_code == 200:
-		out = output.json()
-		return f"The current weather in Cagayan De Oro City is {out['weather'][0]['description']} with a temperature of {out['main']['temp']}°C."
+	try:
+		output = get(f"http://api.openweathermap.org/data/2.5/weather?q=Cagayan%20De%20Oro&appid={APIKey}&units=metric", timeout=2)
+		if output.status_code == 200:
+			out = output.json()
+			return f"The current weather in Cagayan De Oro City is {out['weather'][0]['description']} with a temperature of {out['main']['temp']}°C."
 
-	else:
+		else:
+			return "I apologize, It seems that I am not able fetch the current weather data."
+
+	except exceptions.ConnectionError as e:
 		return "I apologize, It seems that I am not able fetch the current weather data."
